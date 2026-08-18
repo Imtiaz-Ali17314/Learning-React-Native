d0196dbbc3da75c702361d860b44b2e79aaca396
Imtiaz Ali <imtiazali17314@gmail.com>
2026-08-18 16:05:06 +0500

    Refactor code structure and remove redundant sections for improved readability and maintainability

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,15 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View } from "react-native";
 
-export default function Index() {
+export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text>Hello World </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
