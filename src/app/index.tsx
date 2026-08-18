dbf02d6df4de254a759b9eabfb76284c7271cbfa
Imtiaz Ali <imtiazali17314@gmail.com>
2026-08-18 17:07:48 +0500

    feat: add TextInput for user input in HomeScreen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
-import { Alert, ScrollView, Text } from "react-native";
+import { Alert, ScrollView, TextInput } from "react-native";
 
 function HomeScreen() {
   const [isEnabled, setIsEnabled] = useState(false);
   const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+  const [text, setText] = useState("");
 
   const pressMe = () => {
     Alert.alert("Button Pressed!");
   };
   return (
     <ScrollView>
-      {[...Array(60)].map((_, i) => (
+      {/* {[...Array(60)].map((_, i) => (
         <Text key={i} style={{ fontSize: 16 }}>
           Home Screen Item {i + 1}
         </Text>
-      ))}
+      ))} */}
       {/* <Text
         style={{
           fontSize: 20,
@@ -27,6 +28,13 @@ function HomeScreen() {
       </Text>
       <Button title="Click Me" onPress={pressMe} />
       <Switch onValueChange={toggleSwitch} value={isEnabled} /> */}
+      <TextInput
+        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+        placeholder="Enter text here"
+        value={text}
+        onChangeText={setText}
+        keyboardType="phone-pad"
+      />
     </ScrollView>
   );
 }
