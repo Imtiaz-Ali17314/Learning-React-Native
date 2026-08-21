616e29260400ea7f4e6df84ae71bcc92d7073fe4
Imtiaz Ali <imtiazali17314@gmail.com>
2026-08-21 09:52:59 +0500

    use of  FlatList inside SafeAreaView

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import {
   Alert,
+  FlatList,
   Image,
   ImageBackground,
   Pressable,
-  ScrollView,
   StyleSheet,
   Text,
   TextInput,
+  View,
 } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 function HomeScreen() {
   const [isEnabled, setIsEnabled] = useState(false);
@@ -24,14 +26,39 @@ function HomeScreen() {
   };
 
   const image = { uri: "https://legacy.reactjs.org/logo-og.png" };
+
+  const DATA = [
+    {
+      id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+      title: "First Item",
+    },
+    {
+      id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+      title: "Second Item",
+    },
+    {
+      id: "58694a0f-3da1-471f-bd96-145571e29d72",
+      title: "Third Item",
+    },
+  ];
+
+  type ItemProps = { title: string };
+
+  const Item = ({ title }: ItemProps) => (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+
   return (
-    <ScrollView>
-      {/* {[...Array(60)].map((_, i) => (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        {/* {[...Array(60)].map((_, i) => (
         <Text key={i} style={{ fontSize: 16 }}>
           Home Screen Item {i + 1}
         </Text>
       ))} */}
-      {/* <Text
+        {/* <Text
         style={{
           fontSize: 20,
           fontWeight: "bold",
@@ -44,38 +71,47 @@ function HomeScreen() {
       <Button title="Click Me" onPress={pressMe} />
       <Switch onValueChange={toggleSwitch} value={isEnabled} /> */}
 
-      <TextInput
-        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-        placeholder="Enter text here........"
-        value={text}
-        onChangeText={setText}
-        keyboardType="default"
-        multiline
-        numberOfLines={4}
-      />
+        <TextInput
+          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+          placeholder="Enter text here........"
+          value={text}
+          onChangeText={setText}
+          keyboardType="default"
+          multiline
+          numberOfLines={4}
+        />
 
-      <Pressable onLongPress={onPressFunction}>
-        <Text>Press Me</Text>
-      </Pressable>
+        <Pressable onLongPress={onPressFunction} style={styles.pressable}>
+          <Text>Press Me</Text>
+        </Pressable>
 
-      <Image
-        source={{
-          uri: "https://reactnative.dev/img/tiny_logo.png",
-        }}
-        style={{ width: 100, height: 100 }}
-        resizeMode="stretch"
-      />
+        <Image
+          source={{
+            uri: "https://reactnative.dev/img/tiny_logo.png",
+          }}
+          style={{ width: 100, height: 100 }}
+          resizeMode="stretch"
+        />
 
-      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-        <Text style={styles.text}>Inside</Text>
-      </ImageBackground>
-    </ScrollView>
+        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+          <Text style={styles.text}>Inside</Text>
+        </ImageBackground>
+
+        <FlatList
+          data={DATA}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Item title={item.title} />}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    gap: 24,
+    padding: 50,
   },
   image: {
     flex: 1,
@@ -88,6 +124,21 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     backgroundColor: "#000000c0",
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+  pressable: {
+    backgroundColor: "green",
+    padding: 10,
+    borderRadius: 5,
+    alignItems: "center",
   },
 });
 
